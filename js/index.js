1c1766b4947805fd2ba5b458f52f9d87765b5c18
Bruno Oviedo <brunooviedo212@gmail.com>
2023-06-18 23:58:06 -0400

    Add slideshow

---
+++ js/index.js
@@ -0,0 +1,70 @@
+window.sr = ScrollReveal();
+  sr.reveal('.navbar', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-50px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.presentacion', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-100px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.container', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-100px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.informaciones', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-100px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.circle', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-100px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.informaciones2', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+  
+  window.sr = ScrollReveal();
+  sr.reveal('.informaciones3', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-100px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.titulo3', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-100px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.informaciones4', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-100px'
+  });
+
+  
+
+
+
+
+
+
